3469dfd4f8d7f0f7f74c6485f6b822fedfac867b
jungi77777 <jungi77777@gmail.com>
2025-05-07 17:43:05 +0900

    no message

--- MustHaveJSP/src/main/java/common/MyClass.java
+++ MustHaveJSP/src/main/java/common/MyClass.java
@@ -2,12 +2,11 @@ package common;
 
 public class MyClass
 {
-	public static int myFunc(int a,int b)
-	{
+	// 정수 a부터 b까지의 합을 반환하는 정적메서드
+	public static int myFunc(int a, int b) {
 		int sum = 0;
-		for(int i = a; i<b;i++)
-		{
-			sum+= i;
+		for(int i = a ; i< b; i++) {
+			sum += i;
 		}
 		return sum;
 	}
--- MustHaveJSP/src/main/webapp/02ImplicitObject/ResponseWelcome.jsp
+++ MustHaveJSP/src/main/webapp/02ImplicitObject/ResponseWelcome.jsp
@@ -7,6 +7,7 @@
 	<title>내장 객체 - response</title>
 	</head>
 	<body>
+		<img src="../images/welcome.jpg" width="400px" alt="환영합니다!!!" />
 		<h2>로그인 성공</h2>
 	</body>
 </html>
